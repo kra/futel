62921ac7e4dfe71de284f139893ee212997a84fb
Karl Anderson <kra@monkey.org>
2023-01-14 20:50:45 -0800

    dummy config

---
+++ admin/twilio_direct/sns-client-config.js
@@ -0,0 +1,11 @@
+exports.config = {
+    region: 'us-west-2',
+    credentials: {
+        accessKeyId: 'XXX',
+        secretAccessKey: 'XXX'}};
+
+exports.topicArn = 'XXX';
+
+// awsArn = '{{eventlistener_conf_aws_arn}}'
+// awsAccessKeyId = '{{eventlistener_conf_aws_access_key_id}}'
+// awsSecretAccessKey = '{{eventlistener_conf_aws_secret_access_key}}'
